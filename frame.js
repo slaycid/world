5b9072a4141b6d0c7cdc24c47f6461092be444b9
Alex ✶ Varganov <67458241+slaycid@users.noreply.github.com>
2021-06-13 20:20:01 +0500

    Update frame.js

--- frame.js
+++ frame.js
@@ -1,24 +1,11 @@
-/**
- * Provides requestAnimationFrame in a cross browser way.
- * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
- */
-
-
-
 if ( !window.requestAnimationFrame ) {
-/*
-	if ( window.mozRequestAnimationFrame ) alert("has mozRequestAnimationFrame");
-	if ( window.webkitRequestAnimationFrame ) alert("has webkitRequestAnimationFrame");
-	if ( window.oRequestAnimationFrame ) alert("has oRequestAnimationFrame");
-	if ( window.msRequestAnimationFrame ) alert("has msRequestAnimationFrame");
-*/
 	window.requestAnimationFrame = ( function() {
 
 		return window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame || // comment out if FF4 is slow (it caps framerate at ~30fps: https://bugzilla.mozilla.org/show_bug.cgi?id=630127)
+		window.mozRequestAnimationFrame ||
 		window.oRequestAnimationFrame ||
 		window.msRequestAnimationFrame ||
-		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+		function( callback, element ) {
 
 			window.setTimeout( callback, 1000 / 60 );
 
@@ -27,9 +14,5 @@ if ( !window.requestAnimationFrame ) {
 	} )();
 
 }
-/*
-else
+
 {
-	alert("has requestAnimationFrame");
-}
-*/
