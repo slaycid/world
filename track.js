f156d6b6d63959c9f740c97d43f4d9b42e71dbd0
Alex ✶ Varganov <67458241+slaycid@users.noreply.github.com>
2021-06-13 20:18:54 +0500

    Update track.js

--- track.js
+++ track.js
@@ -7,11 +7,11 @@ var touched = false,
 		tw, th, tratio;
 
 function track_init( e ) {
-		// set-up touch interaction
+		
 		document.addEventListener( 'touchstart', track_touch_start, false );
 		document.addEventListener( 'touchmove', track_touch_move, false );
 		document.addEventListener( 'touchend', track_touch_end, false );
-		// set-up mouse interaction
+		
 		document.addEventListener( 'mousedown', track_mouse_down, false );
 		document.addEventListener( 'mousemove', track_mouse_move, false );
 		document.addEventListener( 'mouseup', track_mouse_up, false );
@@ -19,16 +19,8 @@ function track_init( e ) {
 		tw = window.innerWidth/2;
 		th = window.innerHeight/2;
 		tratio = tw / th;
-//		tw /= ratio;
-/*
-		cw /= 2;
-		ch /= 2;
-*/
+	
 }
-
-/*******************************************************************/
-/* HANDLE TOUCH INPUT **********************************************/
-/*******************************************************************/
 
 function track_touch_start(event) 
 {
@@ -82,12 +74,6 @@ function track_touch_end(event)
 	return true;
 }
 
-
-
-/*******************************************************************/
-/* HANDLE MOUSE INPUT **********************************************/
-/*******************************************************************/
-
 function track_mouse_down( event ) 
 {
 	if ( !touched )
@@ -121,12 +107,6 @@ function track_mouse_up( event )
 	return true;
 }
 
-
-/*******************************************************************/
-/* ABSTRACT INPUT HANDLING *****************************************/
-/*******************************************************************/
-
-
 function track_start( x, y )
 {
 	touched = true;
@@ -137,7 +117,6 @@ function track_move( x, y )
 {
 	touchX = (x / tw - 1)*tratio;
 	touchY = -(y / th - 1);
-	//console.log( touchX + " -- " + touchY );
 }
 
 function track_end()
