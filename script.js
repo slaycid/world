29560fc744ead660aa126f909abb6a45eb3c55d8
Alex ✶ Varganov <67458241+slaycid@users.noreply.github.com>
2021-06-15 21:38:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ import { RenderPass } from "https://unpkg.com/three@0.120.0/examples/jsm/postpro
 import { UnrealBloomPass } from "https://unpkg.com/three@0.120.0/examples/jsm/postprocessing/UnrealBloomPass.js";
 import { OBJLoader } from "https://unpkg.com/three@0.120.0/examples/jsm/loaders/OBJLoader";
 import { OrbitControls } from "https://unpkg.com/three@0.120.0/examples/jsm/controls/OrbitControls";
-var cardtemplate = "https://raw.githubusercontent.com/pizza3/asset/master/cardtemplate3.png";
+var cardtemplate = "/assets/card_front.png";
 var cardtemplateback = "https://raw.githubusercontent.com/pizza3/asset/master/cardtemplateback4.png";
-var flower = "card_logo.png";
+var flower = "/assets/card_logo.png";
 var noise2 = "https://raw.githubusercontent.com/pizza3/asset/master/noise2.png";
 var color11 = "https://raw.githubusercontent.com/pizza3/asset/master/color11.png";
 var backtexture = "https://raw.githubusercontent.com/pizza3/asset/master/color3.jpg";
